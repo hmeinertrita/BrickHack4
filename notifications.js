02c89c4dbf5dedc42dfe4c03729e64630f0f3bf2
HarshaPillai <harsha.pillai98@gmail.com>
2018-01-27 20:37:26 -0500

    Made notification when alarm goes off

--- notifications.js
+++ notifications.js
@@ -1,5 +1,5 @@
-function resetTimer(id, milliseconds) {
-  chrome.alarms.create(id,{when: milliseconds});
+function resetTimer(id, minutes) {
+  chrome.alarms.create(id,{delayInMinutes: minutes});
 }
 
 function notify(message) {
@@ -7,5 +7,19 @@ function notify(message) {
 }
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-  notify(alarm.name);
+  //notify(alarm.name);
+  chrome.alarms.create(alarm.notifName. {delayInMinutes: 0.1. periodinMinutes: 0.1});
+
 });
+
+(function(){
+  'use strict';
+  var alarmName = 'remindme';
+  function createAlarm() {
+     chrome.alarms.create(alarmName, {
+       delayInMinutes: 0.1, periodInMinutes: 0.1});
+   }
+   function cancelAlarm() {
+     chrome.alarms.clear(alarmName);
+   }
+})();
