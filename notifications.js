e6a34cb56d970c502d10a3edcd8fb17db7161daa
HarshaPillai <harsha.pillai98@gmail.com>
2018-01-27 22:37:45 -0500

    added alerts

--- notifications.js
+++ notifications.js
@@ -6,14 +6,12 @@ function notify(message) {
     alert(message);
 }
 
-alert("Before");
 chrome.alarms.onAlarm.addListener((alarm) => {
-  console.log("ALARM!!");
   chrome.notifications.create('reminder', {
         type: 'basic',
         iconUrl: 'https://avatars.githubusercontent.com/u/1407390?',
         title: 'Dont forget!',
         message: 'You have things to do'
-     }, function(notificationId) {console.log("created")});
+     }, function(notificationId) {alert("created")});
 });
 alert("After");
